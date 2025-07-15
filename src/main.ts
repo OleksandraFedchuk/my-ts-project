1a791fe08a5b766a8c97bcce9b4d7740a5dff098
Oleksandra <sunnyem@seznam.cz>
2025-07-15 03:09:32 +0200

    N

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,34 @@
-import './style.css'
+import "./style.css";
 
+interface User {
+  name: string;
+  age?: number; // Це поле може бути відсутнім
+}
+
+const poly: User = {
+  name: "Poly",
+};
+
+console.log(poly.age); // ✅ Не буде помилки, оскільки age може бути відсутнім
+
+const jacob: User = {
+  name: "Jacob",
+  age: 36,
+};
+
+console.log(jacob.age);
+console.log(poly.name);
+console.log(poly.age);
+
+interface User {
+  readonly id: number;
+  name: string;
+}
+
+const paul: User = {
+  id: 37,
+  name: "Paul",
+};
+
+console.log(paul.name);
+console.log(paul.id);
