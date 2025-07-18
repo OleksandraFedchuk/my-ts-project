ba79ba83a582a8d05d2a9eca71afefbce0458864
Oleksandra <sunnyem@seznam.cz>
2025-07-18 05:12:03 +0200

    N2

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,52 @@
 import "./style.css";
 
+// interface User {
+//   name: string;
+//   age?: number; // Це поле може бути відсутнім
+// }
+
+// const poly: User = {
+//   name: "Poly",
+// };
+
+// console.log(poly.age); // ✅ Не буде помилки, оскільки age може бути відсутнім
+
+// const jacob: User = {
+//   name: "Jacob",
+//   age: 36,
+// };
+
+// console.log(jacob.age);
+// console.log(poly.name);
+// console.log(poly.age);
+
+// interface User {
+//   readonly id: number;
+//   name: string;
+// }
+
+// const paul: User = {
+//   id: 37,
+//   name: "Paul",
+// };
+
+// console.log(paul.name);
+// console.log(paul.id);
+
 interface User {
+  id: number;
   name: string;
-  age?: number; // Це поле може бути відсутнім
 }
 
-const poly: User = {
-  name: "Poly",
+const getUserName = (users: User[]): string[] => {
+  return users.map((user) => user.name);
 };
 
-console.log(poly.age); // ✅ Не буде помилки, оскільки age може бути відсутнім
+const userList: User[] = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+  { id: 3, name: "Charlie" },
+];
 
-const jacob: User = {
-  name: "Jacob",
-  age: 36,
-};
-
-console.log(jacob.age);
-console.log(poly.name);
-console.log(poly.age);
-
-interface User {
-  readonly id: number;
-  name: string;
-}
-
-const paul: User = {
-  id: 37,
-  name: "Paul",
-};
-
-console.log(paul.name);
-console.log(paul.id);
+const names = getUserName(userList);
+console.log(names);
